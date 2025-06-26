8509cf2311fafbcba338d72605177ae64992b1fd
elis-elis <elisprostotak@gmail.com>
2025-06-26 20:17:43 +0200

    feat(ar_main.py): added text erasing and drawing to the screen, and the color, changed the webcam number, added mode for erasing and drawing

--- ar_main.py
+++ ar_main.py
@@ -11,7 +11,7 @@ HAND_COLORS = {"Left": (0, 255, 0), "Right": (255, 182, 193)}
 DRAW_THICKNESS = 5
 
 # Initialize modules
-cap = cv2.VideoCapture(1)  # Open webcam
+cap = cv2.VideoCapture(2)  # Open webcam
 tracker = HandTracker()  # Hand tracking module
 canvas = None  # Drawing canvas (initialized later)
 prev_positions = {"Left": None, "Right": None}  # Store previous hand position
@@ -42,19 +42,31 @@ while cap.isOpened():
 
             if index_finger and middle_finger:  # Ensure both fingers are detected
                 erased = handle_erase_if_close(canvas, index_finger, middle_finger, DRAW_THRESHOLD, ERASER_SIZE)
+                
                 if erased:
-                    # reset drawing after an erase
-                    prev_positions[hand_type] = None
+                    mode = "erasing"
+                    prev_positions[hand_type] = None  # stop drawing while erasing
                 else:
+                    mode = "drawing"
                     prev = prev_positions[hand_type]
                     if prev:
                         canvas.draw_line(prev, index_finger, current_color, DRAW_THICKNESS)
                     prev_positions[hand_type] = index_finger
 
+        # show mode on screen
+        height = frame.shape[0]
+        if mode == "erasing":
+            cv2.putText(frame, "erasing when index + middle fingers are together", (20, height - 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+        elif mode == "drawing":
+            cv2.putText(frame, "drawing", (20, height - 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+    
     frame = canvas.merge_with_frame(frame)
-    cv2.imshow("AugmentedReality Drawing App", frame)
+
     cv2.rectangle(frame, (10, 10), (60, 60), current_color, -1)
-    cv2.putText(frame, "Color", (10, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1)
+    cv2.putText(frame, "color", (10, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1)
+    
+    cv2.imshow("AugmentedReality Drawing App", frame)
+
 
     key = cv2.waitKey(1) & 0xFF
     if key == ord('c'):
